2b48278df6cd08d3d90f2be5d2da61682756efb3
nilsonsasaki <nilsonsasaki@gmail.com>
2021-11-05 14:11:03 -0300

    updated binding to a lateinit var

--- app/src/main/java/com/nilsonsasaki/travelexpenses/MainActivity.kt
+++ app/src/main/java/com/nilsonsasaki/travelexpenses/MainActivity.kt
@@ -5,9 +5,12 @@ import android.os.Bundle
 import com.nilsonsasaki.travelexpenses.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
     }

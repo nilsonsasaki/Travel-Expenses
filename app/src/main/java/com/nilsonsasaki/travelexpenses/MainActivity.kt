79877a989edddf9a3a2706af586758abe7cd2272
nilsonsasaki <nilsonsasaki@gmail.com>
2021-12-22 08:30:22 -0300

    added ViewModel instance

--- app/src/main/java/com/nilsonsasaki/travelexpenses/MainActivity.kt
+++ app/src/main/java/com/nilsonsasaki/travelexpenses/MainActivity.kt
@@ -3,10 +3,12 @@ package com.nilsonsasaki.travelexpenses
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.nilsonsasaki.travelexpenses.databinding.ActivityMainBinding
+import com.nilsonsasaki.travelexpenses.viewmodels.TravelExpensesViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val viewModel = TravelExpensesViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

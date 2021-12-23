fa3b00276169315ecee98e02f221b9196de65dac
nilsonsasaki <nilsonsasaki@gmail.com>
2021-12-23 14:15:42 -0300

    added a default value and updated distance to a float

--- app/src/main/java/com/nilsonsasaki/travelexpenses/models/TravelValues.kt
+++ app/src/main/java/com/nilsonsasaki/travelexpenses/models/TravelValues.kt
@@ -1,8 +1,8 @@
 package com.nilsonsasaki.travelexpenses.models
 
 data class TravelValues(
-    val distance: Int,
-    val gasPrice: Float,
-    val autonomy: Float,
-    val cost: Float
+    val distance: Float = 0f,
+    val gasPrice: Float = 0f,
+    val autonomy: Float = 0f,
+    val travelCost: Float = 0f
 )

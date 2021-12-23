56f6ebb248f7079307516cdb08c075d0379fffba
nilsonsasaki <nilsonsasaki@gmail.com>
2021-12-23 14:16:52 -0300

    added toast message and travel values live data

--- app/src/main/java/com/nilsonsasaki/travelexpenses/viewmodels/TravelExpensesViewModel.kt
+++ app/src/main/java/com/nilsonsasaki/travelexpenses/viewmodels/TravelExpensesViewModel.kt
@@ -1,7 +1,16 @@
 package com.nilsonsasaki.travelexpenses.viewmodels
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.nilsonsasaki.travelexpenses.R
+import com.nilsonsasaki.travelexpenses.models.TravelValues
 
 class TravelExpensesViewModel : ViewModel() {
 
+    val toastMessage = MutableLiveData<Int>()
+    val travelValues: MutableLiveData<TravelValues> = MutableLiveData<TravelValues>()
+
+    fun validationToast() {
+        toastMessage.value = R.string.testToast //testing observable
+    }
 }

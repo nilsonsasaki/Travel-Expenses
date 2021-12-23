fa67ef80789f3766f07ce1930c8ca9462752b0a1
nilsonsasaki <nilsonsasaki@gmail.com>
2021-12-23 14:20:53 -0300

    added travelValues and toastMessage observer

--- app/src/main/java/com/nilsonsasaki/travelexpenses/MainActivity.kt
+++ app/src/main/java/com/nilsonsasaki/travelexpenses/MainActivity.kt
@@ -2,6 +2,7 @@ package com.nilsonsasaki.travelexpenses
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.nilsonsasaki.travelexpenses.databinding.ActivityMainBinding
 import com.nilsonsasaki.travelexpenses.viewmodels.TravelExpensesViewModel
 
@@ -14,6 +15,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.tvResult.text = getString(R.string.br_price_value,0.00)
+
+        viewModel.travelValues.observe(this,{ newValue->
+            binding.etDistance.setText(newValue.distance.toString())
+            binding.etPrice.setText(newValue.gasPrice.toString())
+            binding.etAutonomy.setText(newValue.autonomy.toString())
+            binding.tvResult.text = getString(R.string.br_price_value,newValue.travelCost)
+
+        })
+
+        viewModel.toastMessage.observe(this, { res ->
+            if (res != null) {
+                val message = getString(res)
+                Toast.makeText(applicationContext, message, Toast.LENGTH_LONG).show()
+            }
+        })
+
+        binding.btCalculate.setOnClickListener {
+            viewModel.validationToast()
+        }
 
     }
 }
